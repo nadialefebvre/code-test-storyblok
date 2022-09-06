da46eedada185cb0579f73df74474985d5ecc470
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-09-06 23:06:45 +0200

    change share links

--- components/Share.js
+++ components/Share.js
@@ -5,6 +5,12 @@ import styles from "../styles/Share.module.css"
 import shared_styles from "../styles/Event.module.css"
 
 const Share = ({ blok }) => {
+
+  const getURL = () => {
+    return window.location.href
+  }
+  const urlPage = getURL()
+
   return (
     <div {...storyblokEditable(blok)} className={styles.section}>
       <h3 className={shared_styles.tiny_heading}>{blok.title}</h3>
@@ -12,7 +18,8 @@ const Share = ({ blok }) => {
       <div className={styles.icons_wrapper}>
         <a
           className={styles.item}
-          href={blok.facebook.url}
+          href={`https://www.facebook.com/sharer/sharer.php?u=${urlPage}`}
+          // href={blok.facebook.url}
           rel="noopener noreferrer"
           target="_blank"
         >
@@ -24,7 +31,8 @@ const Share = ({ blok }) => {
 
         <a
           className={styles.item}
-          href={blok.linkedin.url}
+          href={`https://www.linkedin.com/shareArticle?mini=true&url=https%3A//www.tobii.com/${urlPage}`}
+          // href={blok.linkedin.url}
           rel="noopener noreferrer"
           target="_blank"
         >
@@ -36,7 +44,8 @@ const Share = ({ blok }) => {
 
         <a
           className={styles.item}
-          href={blok.twitter.url}
+          href={`https://twitter.com/intent/tweet?text=An%20event%20by%20Tobii%20${urlPage}`}
+          // href={blok.twitter.url}
           rel="noopener noreferrer"
           target="_blank"
         >
@@ -47,7 +56,8 @@ const Share = ({ blok }) => {
         </a>
 
         {blok.copy_url &&
-          <button onClick={() => navigator.clipboard.writeText(window.location.href)}>
+          // <button onClick={() => navigator.clipboard.writeText(window.location.href)}>
+          <button onClick={() => navigator.clipboard.writeText(urlPage)}>
             <img
               alt="link icon"
               src="https://a.storyblok.com/f/173422/x/5016188e16/link.svg"
